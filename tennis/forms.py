08f0928323a3914fae1cfbdbb191918612b44f0b
sarahlevins <sarahjeanlevins@gmail.com>
2020-01-27 16:01:15 +0800

    cleaned up form validations and included a check to prevent duplicate players

--- tennis/forms.py
+++ tennis/forms.py
@@ -6,22 +6,24 @@ class AddPlayerForm(ModelForm):
     class Meta:
         model = Player
         fields = ['first_name', 'last_name', 'nationality']
-    
-    def clean_first_name(self):
-        first_name = self.cleaned_data['first_name']
-        if first_name.isalpha() == False:
-            raise ValidationError("Player name can be letters only. Please run letters together.")
-        else:
-            first_name = self.cleaned_data['first_name']
-            return first_name.capitalize()
 
-    def clean_last_name(self):
-        last_name = self.cleaned_data['last_name']
-        if last_name.isalpha() == False:
-            raise ValidationError("Player name can be letters only. Please run letters together.")
-        else:
-            last_name = self.cleaned_data['last_name']
-            return last_name.capitalize()
-    
+    def clean(self):
+        cleaned_data = super(AddPlayerForm, self).clean()
+        print(cleaned_data)
+        print(cleaned_data.get('first_name'))
+        first_name = cleaned_data.get('first_name').capitalize()
+        last_name = cleaned_data.get('last_name').capitalize()
+        try:
+            Player.objects.get(first_name = first_name, last_name = last_name)
+            raise ValidationError('This player with that name is already in the system')
+        except Player.DoesNotExist:
+                if first_name.isalpha() == False:
+                    raise ValidationError('Player name can be letters only. Please run letters together.')
+                elif last_name.isalpha() == False:
+                    raise ValidationError('Player name can be letters only. Please run letters together.')
+                else:
+                    self.cleaned_data['first_name'] = first_name
+                    self.cleaned_data['last_name'] = last_name
+                    return cleaned_data
 
     
